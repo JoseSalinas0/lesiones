7d624dcd11a5a18c4beb344acc564ef12a2299c3
JoseSalinas <104866166+JoseSalinas0@users.noreply.github.com>
2022-12-11 21:24:05 -0600

    Update c.php

--- c.php
+++ c.php
@@ -5,10 +5,11 @@ $con=conectar();
 # recibir datos
 $us=$_POST['user'];
 $com=$_POST['com'];
+$fecha=$_POST['fecha'];
 
 # ingresarlos a la base de datos
-if ($us!=null && $com!=null) {
-	$sql="insert into comentarios(usuario, comentario)values('".$us."','".$com."')";
+if ($us!=null && $com!=null && $fecha!=null) {
+	$sql="insert into comentarios(usuario, comentario, fecha)values('".$us."','".$com."','".$fecha."')";
 	mysqli_query($con,$sql);
 	if ($user=1) {
 		header("location:index.php?usuario=".$us);
