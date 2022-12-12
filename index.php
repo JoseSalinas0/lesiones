238526902aea0394ede742883e547e771294a580
JoseSalinas <104866166+JoseSalinas0@users.noreply.github.com>
2022-12-11 21:23:06 -0600

    Update index.php

--- index.php
+++ index.php
@@ -92,7 +92,12 @@ if(isset($_GET['usuario'])){
     </footer>
     <!--- JS --->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="libs/moment/moment.min.js"></script>
     <script>
+        var fecha = moment().format('MMM Do YY');
+        var fec=document.getElementById("fec");
+        console.log(fecha);
+        fec.value=fecha;
         var usuario="<?php echo $usu?>";
         var cs=document.getElementById("cs");
         var is=document.getElementById("is");
